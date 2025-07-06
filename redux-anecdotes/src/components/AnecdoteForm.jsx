0ed1b59142bfedf82c5b0f5bb25148e430969c0a
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-07-06 13:26:47 -0300

    exercise 6.15 done -- creation of anecdotes on the backend is working now

--- redux-anecdotes/src/components/AnecdoteForm.jsx
+++ redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -2,13 +2,17 @@ import { useDispatch } from 'react-redux';
 import { createAnecdote } from '../reducers/anecdoteReducer';
 import { showNotification } from '../reducers/notificationReducer';
 
+import anecdoteService from './../services/anecdotes';
+
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
-  const handleAddAnecdote = (event) => {
+  const handleAddAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = '';
-    dispatch(createAnecdote(content));
+
+    const newAnecdote = await anecdoteService.createNew(content);
+    dispatch(createAnecdote(newAnecdote));
     dispatch(showNotification(`Created note ${content}`));
   };
   return (
